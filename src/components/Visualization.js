2a2f9affc070f3e06508507b8f0fe360a56d1f0f
SchillingSebastian <sebastian.schilling@aol.com>
2020-01-05 16:51:15 +0100

    Visualization now working on direct out: plot.pkl

--- src/components/Visualization.js
+++ src/components/Visualization.js
@@ -1,54 +1,146 @@
 import React, { Component } from "react";
 import { Button, Select, Row, Col } from "antd";
-import Chart from "chart.js";
+import { Chart } from "react-google-charts";
 import classes from "./LineGraph.module.css";
 const { Option } = Select;
-const preselectedTestGroups = [];
 
 export default class myLineGraph extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      chartData: null,
+      isLoading: false,
+      error: null,
+      selectedCat: ["OVERALL TIME"]
+    };
+  }
+
   chartRef = React.createRef();
 
   componentDidMount() {
-    const myChartRef = this.chartRef.current.getContext("2d");
     console.log("Mounted + creates options");
-    for (let i = 10; i < 36; i++) {
-      preselectedTestGroups.push(
-        <Option key={i.toString(36) + i}>{i.toString(36) + i}</Option>
-      );
-    }
 
-    new Chart(myChartRef, {
-      type: "line",
-      data: {
-        //Bring in data
-        labels: ["Jan", "Feb", "March"],
-        datasets: [
-          {
-            label: "Sales",
-            data: [86, 67, 91]
-          }
-        ]
-      },
-      options: {
-        //Customize chart options
-      }
-    });
+    this.setState({ isLoading: true });
+    fetch("api/giveMeSample")
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error("Error in fetching API Data");
+        }
+      })
+      .then(json => this.setState({ chartData: json, isLoading: false }))
+      .catch(error => this.setState({ error, isLoading: false }));
+    //setting up options
+    if (this.state.chartData != null) {
+    }
   }
 
   render() {
+    const { isLoading, error, selectedCat } = this.state;
+    if (error) {
+      return (
+        <div>
+          <p>Error see:</p>
+          <p>{error.message}</p>
+        </div>
+      );
+    }
+    if (isLoading) {
+      return (
+        <div>
+          <p>Is Loading</p>
+        </div>
+      );
+    }
+    if (selectedCat.length == 0) {
+    }
+
+    if (this.state.chartData != null) {
+      return this.pageDataLoaded();
+    } else {
+      return <div>null</div>;
+    }
+  }
+
+  pageDataLoaded() {
+    const { chartData } = this.state;
+    //actual Data construction
+    //retrieve actual data
+    var tempArray = [];
+    Object.keys(chartData).forEach(function(key) {
+      tempArray.push(chartData[key]);
+    });
+    var data = tempArray[0];
+    //retrieve possible Display Options
+    var times = [];
+    for (let name in data) {
+      times.push(<Option key={name.toString()}>{name.toString()}</Option>);
+    }
+    //retrieve testnames
+    var testnamesData = data.AGGREGATIONS;
+    var testnames = [];
+    for (let name in testnamesData) {
+      testnames.push(name);
+    }
+    //construct chart data array
+    var actualChartData = [];
+    var indexes = ["Instances"].concat(this.state.selectedCat);
+    actualChartData[0] = indexes;
+    for (let i = 0; i < testnames.length; i++) {
+      //add name
+      var testinstanceData = [];
+      testinstanceData.push(testnames[i]);
+      //add Data in correspondance to selected Values
+      var selections = this.state.selectedCat;
+      for (let j = 0; j < selections.length; j++) {
+        var instanceData = data[selections[j]];
+        testinstanceData.push(instanceData[testnames[i]]);
+      }
+      actualChartData.push(testinstanceData);
+    }
+    var chartHeight = window.innerHeight - 200;
+    var chartWidth = window.innerWidth - window.innerWidth * 0.15;
+
+    var displayedName = data.testname;
     return (
       <div className={classes.graphContainer}>
-        <canvas id="myChart" ref={this.chartRef} />
+        <div style={{ display: "flex", maxWidth: 1600 }}>
+          <Chart
+            width={chartWidth}
+            height={chartHeight}
+            chartType="ColumnChart"
+            loader={<div>Loading Chart</div>}
+            data={actualChartData}
+            options={{
+              title: displayedName,
+              chartArea: { width: "80%" },
+              isStacked: false,
+              hAxis: {
+                title: "Instances",
+                minValue: 0
+              },
+              vAxis: {
+                title: "Time"
+              }
+            }}
+            // For tests
+            rootProps={{ "data-testid": "3" }}
+          />
+        </div>
         <Row type="flex">
           <Col span={15}>
             <Select
               style={{ width: "100%" }}
               mode="multiple"
               placeholder="Please Select"
-              defaultVale={["goodInstances"]}
-              onChange={this.handleChanges}
+              defaultValue={this.state.selectedCat}
+              onChange={value => {
+                //console.log(value);
+                this.setState({ selectedCat: value });
+              }}
             >
-              {preselectedTestGroups}
+              {times}
             </Select>
           </Col>
           <Col span={1}>
@@ -59,7 +151,8 @@ export default class myLineGraph extends Component {
     );
   }
 
-  handleChange(value) {
+  handleChanges(value) {
     console.log(`selected ${value}`);
+    console.log(this.state.selectedCat);
   }
 }
